28839d98bbbeebf98dad15d0c16dd153fb23871a
ANANDHAKUMAR18 <101105244+ANANDHAKUMAR18@users.noreply.github.com>
2024-06-30 18:50:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,3 @@ resource "aws_vpc" "ak-vpc" {
     Name = "AK-VPC"
   }
 }
-
-resource "aws_subnet" "ak-public-subnet" {
-  vpc_id = aws_vpc.ak-vpc.id
-  cidr_block = var.aws_cidr_block_subnet
-  availability_zone = var.aws_availability_zone
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "AK-SN"
-  }
-}
